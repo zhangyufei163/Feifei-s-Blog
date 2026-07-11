46905f2fade2af87f7c0bb018637518a23637de2
zhangyufei163 <2579320842@qq.com>
2026-07-11 23:17:20 +0800

    chore: 清理自带演示文章并整理项目结构

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "avatar.png",
 
 	// 名字
-	name: "Firefly",
+	name: "Feifeijiang",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm Feifeijiang.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
